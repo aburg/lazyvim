626101f2dfbe8a302718293308f497e0a2d79ac4
Andreas Burg <andreas.burg@gmx.net>
2023-09-12 18:11:53 +0200

    buffer handling keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set('n', '<leader>wb', '<cmd>w<cr>', { desc='[W]rite [B]uffer'})
+vim.keymap.set('n', '<leader>cb', '<cmd>bd<cr>', { desc='[C]lose [B]uffer'})
