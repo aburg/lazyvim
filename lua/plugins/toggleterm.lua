df96c62b7b2e2224ddee02ef69b1598044681104
Andreas Burg <andreas.burg@gmx.net>
2023-09-12 17:32:38 +0200

    toggleterm test setup

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,25 @@
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {},
+    config = function()
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local terminals = {
+        top = Terminal:new({ cmd = "top", hidden = true, direction = "float" }),
+        bottom = Terminal:new({ cmd = "bottom", hidden = true, direction = "float" }),
+      }
+
+      function my_term_toggle(terminal)
+        terminals[terminal]:toggle()
+      end
+
+      require("toggleterm").setup()
+
+      vim.keymap.set("n", "<f7>", "<cmd>lua my_term_toggle('top')<cr>", { desc = "top" })
+      vim.keymap.set("n", "<f8>", "<cmd>lua my_term_toggle('bottom')<cr>", { desc = "bottom" })
+    end,
+  },
+}
