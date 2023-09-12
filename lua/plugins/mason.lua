e6de34888e1d213e2eff738aafcff01075445c18
Andreas Burg <andreas.burg@gmx.net>
2023-09-12 12:08:25 +0200

    mason ensure installed

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "ansible-language-server",
+        "emmet-language-server",
+        "intelephense",
+        "svelte-language-server",
+      },
+    },
+  },
+}
